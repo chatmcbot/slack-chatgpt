8ad26450205921b9c75abc41f677c954bf7dccf3
Abhinav Sharma <abhinav@laso.ai>
2023-03-28 21:24:31 -0700

    copy

--- main_prod.py
+++ main_prod.py
@@ -234,7 +234,7 @@ def handler(event, context_):
                     {
                         "type": "input",
                         "block_id": "system_prompt",
-                        "label": {"type": "plain_text", "text": "System Prompt"},
+                        "label": {"type": "plain_text", "text": "Override System Prompt"},
                         "element": {"type": "plain_text_input", "action_id": "input", "initial_value": already_set_system_prompt or SYSTEM_TEXT},
                     },
                 ],
